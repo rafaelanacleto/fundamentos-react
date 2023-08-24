179c3c25632d86a20989070ef6c92b03189f9d48
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2023-08-24 14:00:58 -0300

    Finalizado capitudo inicial

--- src/components/mega/DisplayResult.jsx
+++ src/components/mega/DisplayResult.jsx
@@ -1,35 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const DisplayResult = (props) => {
-  
-  const minNumber = 1;
-  const maxNumber = 50;
-  const numberOfRandomNumbers = props.intervalo;
-  let resultado = new Array();
 
-  function generateUniqueRandomNumbers(count, min, max) {
-    if (count > max - min + 1) {
-      throw new Error(
-        "A contagem de números únicos solicitada é maior do que o intervalo fornecido."
-      );
-    }
-
-    const uniqueNumbers = new Set();
-
-    while (uniqueNumbers.size < count) {
-      const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-      uniqueNumbers.add(randomNumber);
-    }
-
-    resultado = Array.from(uniqueNumbers);
-
-    return Array.from(uniqueNumbers);
-  }
 
   return <div>
-    <button onClick={generateUniqueRandomNumbers(numberOfRandomNumbers, minNumber, maxNumber)}>Gerar numeros</button>
-    <span>
-        {resultado}
-    </span>
+  
   </div>;
 };
--- src/components/mega/MegaSena.jsx
+++ src/components/mega/MegaSena.jsx
@@ -1,20 +1,66 @@
-import React, { useState } from 'react'
-import { DisplayResult } from './DisplayResult';
+import React, { useState } from "react";
 
 export const MegaSena = (props) => {
-  
-    const [intervalo, setIntervalo] = useState(props.intervalo);
-  
-    function setValor (e) {
-        setIntervalo(e.target.value);
+  const [intervalo, setIntervalo] = useState(props.intervalo);
+
+  function setValor(e) {
+    setIntervalo(e.target.value);
+  }
+
+  const minNumber = 1;
+  const maxNumber = 50;
+  const [resultados, setResultado] = useState(Array(props.intervalo || 6).fill(0));
+
+  const generateUniqueRandomNumbers = (count, min, max) => {
+    if (count > max - min + 1) {
+      throw new Error(
+        "A contagem de números únicos solicitada é maior do que o intervalo fornecido."
+      );
     }
-  
-    return (
+
+    const uniqueNumbers = new Set();
+
+    while (uniqueNumbers.size < count) {
+      const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+      uniqueNumbers.add(randomNumber);
+    }
+
+    setResultado(Array.from(uniqueNumbers));
+
+  };
+
+  const liVar = resultados.map((resultado, index) => {
+    return(
+      <li key={index}>
+          {resultado}
+      </li>
+    )
+  });
+ 
+  return (
     <div>
-        <label htmlFor="intervalo"></label>
-         <input type="number" value={intervalo} name="intervalo" id="intervalo" onChange={setValor} />
-         <hr />
-         <DisplayResult intervalo={intervalo} />
+      <label htmlFor="intervalo"></label>
+      <input
+        type="number"
+        value={props.intervalo}
+        name="intervalo"
+        id="intervalo"
+        onChange={setValor}
+      />
+      <hr />
+      <button
+        onClick={() => {
+          generateUniqueRandomNumbers(props.intervalo, minNumber, maxNumber);
+        }}
+      >
+        Gerar numeros
+      </button>
+      <br />
+
+        <ul>
+          {liVar}
+        </ul>
+
     </div>
-  )
-}
+  );
+};
